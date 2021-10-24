46a40d913be07b3ce89a578964e18e614ad91c41
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-10-24 12:19:05 +1100

    default provider is SYSADMIN

--- providers.tf
+++ providers.tf
@@ -4,15 +4,6 @@ provider "snowflake" {
   account   = var.snowflake_account
   region    = var.snowflake_region
 
-  role      = "ACCOUNTADMIN"
-}
-
-provider "snowflake" {
-  username  = var.snowflake_username
-  account   = var.snowflake_account
-  region    = var.snowflake_region
-
-  alias     = "SYSADMIN"
   role      = "SYSADMIN"
 }
 
